071c1498f242d139f896df20b61f8885d6ac1adb
Khalid Salman-Yusuf <57933247+salskhal@users.noreply.github.com>
2023-09-07 08:17:45 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+ document.addEventListener('DOMContentLoaded', function() {
+        // Get the current date and time
+        const now = new Date();
+
+        // Update the current day of the week element
+        const dayOfWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+        dayOfWeekElement.textContent = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(now);
+
+        // Update the current UTC time element
+        const utcTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
+        utcTimeElement.textContent = now.getTime();
+
+    });
